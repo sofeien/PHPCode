161f3ae6b4f97066bd820766a0b22297be85679a
sofeien <444023990@qq.com>
2017-05-22 17:58:38 +0800

    数据库查询

---
+++ demo23.php
@@ -0,0 +1,8 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', 'root', 'cc');
+$result = $mysqli->query("select * from users",MYSQLI_USE_RESULT);
+while($row = $result->fetch_row()) {
+    echo $row[0] . " " . $row[1] ."<br>\n";
+}
+$mysqli->close();
+?>
