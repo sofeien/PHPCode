5587e6b82a017577c65623ef67b3ce42a50cf981
sofeien <444023990@qq.com>
2017-05-22 18:03:15 +0800

    修改查询数据库代码，增加free

--- demo23.php
+++ demo23.php
@@ -4,5 +4,6 @@ $result = $mysqli->query("select * from users",MYSQLI_USE_RESULT);
 while($row = $result->fetch_row()) {
     echo $row[0] . " " . $row[1] ."<br>\n";
 }
+$result->free();
 $mysqli->close();
 ?>
